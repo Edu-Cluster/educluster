1d70f7e6eb9d6a7f1912b5bb27a1495672b88a22
vladudenis <vladudenis09@gmail.com>
2022-12-23 15:58:15 +0100

    [EC-92]: Terminsuche implementiert.

--- components/Item/ItemList.tsx
+++ components/Item/ItemList.tsx
@@ -38,57 +38,61 @@ const ItemList = ({ items, title, placeholder }: Props) => {
       )}
       <div className="h-full w-full overflow-y-auto card flex flex-col justify-between divide-y">
         <div className="h-fit divide-y">
-          {title === 'Cluster'
-            ? // @ts-ignore
-              (items as Cluster)[page - 1].map((item, idx) => (
-                <ClusterItemInList
-                  key={idx}
-                  clustername={item.clustername}
-                  description={item.description}
-                  creator={item.person.username}
-                  link={'/cluster/' + item.clustername + '#' + item.id}
-                ></ClusterItemInList>
-              ))
-            : // @ts-ignore
-              (items as Appointment)[page - 1].map((item, idx) => (
-                <AppointmentItemInList
-                  key={idx}
-                  title={item.title}
-                  description={item.description}
-                  creator={item.item.person.username}
-                  roomname={item.roomname}
-                  link={'/appointment/' + item.name + '#' + item.id}
-                >
-                  {item.topics_for_appointment &&
-                    item.topics_for_appointment.map(
-                      (
-                        tag: {
-                          topic_topicTotopics_for_appointment: {
-                            symbol: string;
-                          };
-                        },
-                        idx: React.Key | null | undefined,
-                      ) => (
-                        <Tag
-                          key={idx}
-                          name={tag.topic_topicTotopics_for_appointment.symbol}
-                        />
-                      ),
-                    )}
-                </AppointmentItemInList>
-              ))}
+          {items &&
+            (title === 'Cluster'
+              ? // @ts-ignore
+                (items as Cluster)[page - 1].map((item, idx) => (
+                  <ClusterItemInList
+                    key={idx}
+                    clustername={item.clustername}
+                    description={item.description}
+                    creator={item.person?.username}
+                    link={'/cluster/' + item.clustername + '#' + item.id}
+                  ></ClusterItemInList>
+                ))
+              : // @ts-ignore
+                (items as Appointment)[page - 1].map((item, idx) => (
+                  <AppointmentItemInList
+                    key={idx}
+                    title={item.name}
+                    description={item.description}
+                    creator={item.person?.username}
+                    roomname={item.roomname}
+                    link={'/appointment/' + item.name + '#' + item.id}
+                  >
+                    {item.topics_for_appointment &&
+                      item.topics_for_appointment.map(
+                        (
+                          tag: {
+                            topic_topicTotopics_for_appointment: {
+                              symbol: string;
+                            };
+                          },
+                          idx: React.Key | null | undefined,
+                        ) => (
+                          <Tag
+                            key={idx}
+                            name={
+                              tag.topic_topicTotopics_for_appointment.symbol
+                            }
+                          />
+                        ),
+                      )}
+                  </AppointmentItemInList>
+                )))}
         </div>
         <div className="py-3 flex justify-center items-center gap-2">
-          {items.map((item, idx) => (
-            <div
-              key={idx + 1}
-              className={`${page == idx + 1 ? 'text-white bg-blue-400' : ''}
+          {items &&
+            items.map((item, idx) => (
+              <div
+                key={idx + 1}
+                className={`${page == idx + 1 ? 'text-white bg-blue-400' : ''}
                   w-6 h-6 rounded-sm text-gray-700 hover:text-white hover:bg-blue-400 slow-animate flex justify-center items-center cursor-pointer text-md`}
-              onClick={loadNewPage}
-            >
-              {idx + 1}
-            </div>
-          ))}
+                onClick={loadNewPage}
+              >
+                {idx + 1}
+              </div>
+            ))}
         </div>
       </div>
     </div>
