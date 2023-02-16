d9faece5050f504c40990e72fca58943265f467e
lara.kammerer <lara.kammerer@gmail.com>
2023-02-16 12:57:02 +0100

    EC-116 Filter zu Raumgröße und Ausstattung
    EC-115 Uhrzeitfelder

--- src/server/controllers/catalog.controller.ts
+++ src/server/controllers/catalog.controller.ts
@@ -1,6 +1,10 @@
 import { TRPCError } from '@trpc/server';
 import { statusCodes } from '../../lib/enums';
-import { readTeachingTimes } from '../services/catalog.service';
+import {
+  readEquipment,
+  readRoomSizes,
+  readTeachingTimes,
+} from '../services/catalog.service';
 
 /** TODO Lara
  * Returns the user object from the context.
@@ -15,6 +19,40 @@ export const getTeachingTimes = async ({}: {}) => {
       status: statusCodes.SUCCESS,
       data: {
         times,
+      },
+    };
+  } catch (err: any) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: err.message,
+      originalError: err,
+    });
+  }
+};
+export const getEquipment = async ({}: {}) => {
+  try {
+    const equipment = await readEquipment();
+    return {
+      status: statusCodes.SUCCESS,
+      data: {
+        equipment,
+      },
+    };
+  } catch (err: any) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: err.message,
+      originalError: err,
+    });
+  }
+};
+export const getRoomSizes = async ({}: {}) => {
+  try {
+    const roomsizes = await readRoomSizes();
+    return {
+      status: statusCodes.SUCCESS,
+      data: {
+        roomsizes,
       },
     };
   } catch (err: any) {
--- src/server/routers/catalog.routes.ts
+++ src/server/routers/catalog.routes.ts
@@ -1,10 +1,21 @@
 import { createRouter } from '../createRouter';
-import { getTeachingTimes } from '../controllers/catalog.controller';
+import {
+  getEquipment,
+  getRoomSizes,
+  getTeachingTimes,
+} from '../controllers/catalog.controller';
 
-export const catalogRouter = createRouter().query('times', {
-  resolve: async ({ ctx }) => await getTeachingTimes({ ctx }),
-  // })
-  // .query('ofCluster', {
-  //     input: clusterSchema,
-  //     resolve: async ({ input }) => await getItemOfClusterHandler({ input }),
-});
+export const catalogRouter = createRouter()
+  .query('times', {
+    resolve: async ({ ctx }) => await getTeachingTimes({ ctx }),
+  })
+  .query('equipment', {
+    resolve: async ({ ctx }) => await getEquipment({ ctx }),
+  })
+  .query('roomsize', {
+    resolve: async ({ ctx }) => await getRoomSizes({ ctx }),
+  });
+// })
+// .query('ofCluster', {
+//     input: clusterSchema,
+//     resolve: async ({ input }) => await getItemOfClusterHandler({ input }),
